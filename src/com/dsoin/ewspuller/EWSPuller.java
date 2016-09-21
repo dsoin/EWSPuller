0f8b3749d2970b81029fde634be656dff83118c7
dsoin <dmitry.soin@gmail.com>
2016-09-21 14:26:04 +0100

    Inbox now cuptured as a folder name

--- src/com/dsoin/ewspuller/EWSPuller.java
+++ src/com/dsoin/ewspuller/EWSPuller.java
@@ -158,6 +158,8 @@ public class EWSPuller {
 
     private static FolderId getFolderId(String folderName, ExchangeService service) throws Exception {
         Folder rootFolder = Folder.bind(service, WellKnownFolderName.Inbox);
+        if (folderName.equals(rootFolder.getDisplayName())
+            return rootFolder.getId();
         for (Folder folder : rootFolder.findFolders(new FolderView(100))) {
             log.info(folder.getDisplayName());
             if (folderName.equalsIgnoreCase(folder.getDisplayName())) {
